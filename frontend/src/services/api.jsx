07fafbecbaeb534210e2b81d59b4944a959446c4
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-01-30 22:40:37 +0530

    add url

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://eventsmanagement-5.onrender.com",
+  baseURL: " https://eventsmanagement6.onrender.com",
   withCredentials: true
 });
 
